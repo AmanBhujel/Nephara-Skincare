a957bb960564a6ecac7252a325f630df15ea3629
Evan Sunde <evansunde2@gmail.com>
2024-02-15 10:43:51 +0545

    improved homepage

--- src/app/landing/HeroSection.tsx
+++ src/app/landing/HeroSection.tsx
@@ -6,24 +6,68 @@ import Link from 'next/link';
 
 const HeroSection = () => {
     return (
-        <div className="flex w-full justify-center items-center border-b-2 bg-white ">
-            <div className="w-[95%] lg:w-[63rem]  xl:w-[79rem] 2xl:w-[90rem] h-auto pb-12 sm:pb-8  xl:h-[46rem]  flex flex-col-reverse lg:flex-row justify-center items-center ">
+        <section>
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
+        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+        
+        <div className="text-center pb-12 md:pb-16">
+        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your Skin <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">WONDERFUL</span></h1>
+        <div className="max-w-3xl mx-auto">
+          <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Book appointments with top south asian dermatologists from the comfort of your home.</p>
 
-                <div className="md:w-[95%] lg:w-[50%] h-full justify-center items-center lg:items-start flex flex-col animatedFromLeft">
-                    <p className="w-full flex flex-wrap text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-extrabold tracking-wide font-montserrat items-center justify-center lg:items-start text-center lg:text-left"> Virtual Dermatology Care</p>
-                    <p className="text-sm md:text-lg xl:text-xl font-medium lg:[95%] xl:w-[90%] flex-wrap mt-2 md:mt-2 lg:mt-8 text-[#665833] text-center lg:text-left">Book appointments with top south asian dermatologists from the comfort of your home</p>
-                    
-                    <div className="flex items-center mt-6 md:mt-8 lg:mt-12">
+
+                    <div className="flex items-center mt-6 md:mt-8 lg:mt-12" data-aos="zoom-y-out" data-aos-delay="300">
                         <Link href={'/appointment'}>
                             <button className="bg-[#8f67e2] hover:bg-[#9c75e9] py-2 px-6 md:py-3  md:px-9 font-semibold text-lg text-white rounded-[8px] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-lg">Book now</button></Link>
                         <button className="ml-8 flex justify-center items-center tracking-tight hover:underline text-sm lg:text-base text-[#d25d7a] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-lg" >About Our Doctors <i className="ml-3 text-3xl"><FaCircleArrowRight /> </i></button>
                     </div>
                 </div>
-                <div className="w-[75%] lg:w-[50%]  h-full animatedFromRight" >
-                    <Image src={Profile} width={500} height={200} className="w-full" alt="Nephara" />
+
+                <div className='flex justify-between mt-6'>
+                    <div className='w-[45%]'>
+                    <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Best Skin Doctor in Nepal</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Best Services</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Bestest Of Quality</span>
+                    </li>
+                  </ul>
+                    </div>
+                </div>
+
+                <div className='flex justify-end w-full'>
+                    <div className="w-[65%] h-full animatedFromRight transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-lg" >
+                        <Image src={Profile} width={500} height={200} className="w-full" alt="Nephara" />
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 };
 
--- src/app/landing/WhatWeBelieve.tsx
+++ src/app/landing/WhatWeBelieve.tsx
@@ -26,17 +26,74 @@ const WhatWeBelieve = () => {
 
     }, []);
     return (
-        <div ref={WhatweBelieveRef} className="flex  w-full justify-center items-center border-b-2 bg-white">
-            <div className={`flex flex-col lg:flex-row-reverse justify-center items-center w-[95%] lg:w-[63rem]  xl:w-[79rem] 2xl:w-[90rem] h-auto py-12 xl:h-[35rem] ${startAnimation?"animatedFromBottom":""}`}>
-                <div className="w-[80%] lg:w-[40%] xl:w-[50%] h-full flex justify-center items-center">
-                    <Image src={Nurse} width={400} height={600} className="w-[80%] bg-contain rounded-[8px] " alt="Nurse" />
+        <section>
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="py-12 md:py-20 border-t border-gray-800">
+    
+              {/* Section header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+                <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Premium care for your skin</div>
+                <h1 className="h2 text-2xl mb-4">Reach Out to our Skin care Specialist now</h1>
+                <p className="text-xl text-gray-400">Skin Doctor with 10 years of experience with Masters Degree</p>
+              </div>
+    
+              {/* Items */}
+              <div className="grid gap-20">
+    
+                {/* 1st item */}
+                <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                  {/* Image */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                    <Image className="max-w-full mx-auto md:max-w-none h-auto" src={"https://cdna.artstation.com/p/assets/images/images/008/265/652/large/siwoo-kim-546-max.jpg"} width={540} height={405} alt="Features 01" />
+                  </div>
+                  {/* Content */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right" data-aos-delay="150">
+                    <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                      <div className="font-architects-daughter text-xl text-purple-600 mb-2">Premium Service and spend Less</div>
+                      <h3 className="h3 mb-3">Low cost Skin care service</h3>
+                      <p className="text-xl text-gray-400 mb-4">Do skin skin check up with our certified Skin specialist doctor</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="lg:w-[57%] xl:w-[50%] h-full flex flex-col justify-center items-center lg:items-start">
-                    <p className="text-3xl text-[#a376ff] font-semibold ml-6 mt-6 lg:mt-0">WHAT WE BELIEVE IN</p>
-                    <p className="text-base md:text-lg tracking-wide w-[95%] md:w-[90%] lg:w-[100%] xl:w-[90%] 2xl:w-[80%] flex flex-wrap lg:ml-6 text-justify mt-2 xl:mt-4">Our core belief is rooted in ensuring your satisfaction. We are dedicated to providing a seamless online dermatologist appointment experience, connecting you with top-notch doctors. Need assistance or have questions? Feel free to reach out to us via email, and our team is committed to responding promptly within a day. <br /> Your convenience and peace of mind matter to us, which is why we offer a flexible refund policy to accommodate any changes or cancellations to your appointments. Trust us for a reliable and customer-centric approach to your healthcare journey.</p>
+    
+                {/* 2nd item */}
+                <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                  {/* Image */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                    <Image className="max-w-full mx-auto md:max-w-none h-auto" src={"https://r2.starryai.com/results/376525370/0f90d74a-d037-436f-b1a2-d3844b10caf1.webp"} width={540} height={405} alt="Features 02" />
+                  </div>
+                  {/* Content */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left"  data-aos-delay="150">
+                    <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                      <div className="font-architects-daughter text-xl text-purple-600 mb-2">Comfort of your home</div>
+                      <h3 className="h3 mb-3">Easy Scheduleing from your home in your home</h3>
+                      <p className="text-xl text-gray-400 mb-4">Talk or video call with our doctor.</p>
+                    </div>
+                  </div>
                 </div>
+    
+                {/* 3rd item */}
+                <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                  {/* Image */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                    <Image className="max-w-full mx-auto md:max-w-none h-auto" src={"https://img.freepik.com/premium-photo/photo-cyberpunk-woman-showing-backfuturistic-clothes_740020-532.jpg"} width={540} height={405} alt="Features 03" />
+                  </div>
+                  {/* Content */}
+                  <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right"  data-aos-delay="150">
+                    <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                      <div className="font-architects-daughter text-xl text-purple-600 mb-2">Our Support</div>
+                      <h3 className="h3 mb-3">Exceptional Support</h3>
+                      <p className="text-xl text-gray-400 mb-4">You can have our out of the world bestest of the best support for free</p>
+                    </div>
+                  </div>
+                </div>
+    
+              </div>
+    
             </div>
-        </div>)
+          </div>
+        </section>
+      )
 };
 
 export default WhatWeBelieve;
