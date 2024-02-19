3e086fcce885ede26d817f868cbec9b8ee176280
AmanBhujel <bhujelaman20@gmail.com>
2024-02-19 23:59:42 +0545

    added useeffect in layout for animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 // import type { Metadata } from 'next'
-
 import { Inter, Architects_Daughter } from 'next/font/google'
 import './globals.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
@@ -8,7 +7,7 @@ import { Toaster } from 'sonner';
 import { NextUIProvider } from "@nextui-org/react";
 import LogoutModal from '@/components/LogoutModal'
 import { getCookie } from '@/components/utils/Cookie'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -37,6 +36,7 @@ export default function RootLayout({
 }) {
   const token = getCookie("token");
   const tokenParts = token.split(" ");
+  const [windowWidth, setWindowWidth] = useState<number>(0);
 
   const headers = {
     authorization: tokenParts ? tokenParts[1] : "",
@@ -52,10 +52,24 @@ export default function RootLayout({
   });
 
   useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
     AOS.init({
       once: false,
-      disable: 'phone',
-      duration: 600,
+      // disable: 'phone',
+      duration: windowWidth > 640 ? 600 : 300,
       easing: 'ease-out-sine',
     })
   }, [])
