0adc566af4925c7c7e07eec9e2396e90c7770aeb
AmanBhujel <bhujelaman20@gmail.com>
2024-02-24 09:17:35 +0545

    fixed build errors

--- src/app/dashboard/settings/components/EditProfile.tsx
+++ src/app/dashboard/settings/components/EditProfile.tsx
@@ -106,26 +106,9 @@ const EditProfile: React.FC<EditProfileProps> = ({ activeSettingButton, setActiv
     };
 
     return (
-<<<<<<< HEAD
-        <div className={` ${windowWidth > 1024 && activeSettingButton !== "change-password" || activeSettingButton === "Edit" ? "flex" : "hidden"} w-full h-auto   lg:w-[70%] xl:w-[50%] justify-center border lg:ml-[5%] bg-[#f6f8fc] overflow-auto max-h-screen `}>
-            <div className='w-[95%] md:w-[75%] xl:w-[70%] 2xl:w-[60%] relative'>
-                <p className='text-2xl sm:text-4xl absolute top-2 left-2 lg:hidden cursor-pointer' onClick={() => setActiveSettingButton("")}><IoArrowBack /></p>
-                <div className='flex w-full justify-center items-center mt-6 lg:mt-8'>
-                    <p className='text-3xl sm:text-4xl font-semibold text-[#743bfb]  tracking-wide'>Edit Your Profile</p>
-                </div>
-                <div className='flex flex-col w-full justify-center items-center mt-3'>
-                    <Image src={UserInfo[0]?.photo ? UserInfo[0].photo : ''} width={200} height={200} alt='Profile Image' className='w-32 h-32 rounded-[8px] border object-cover' placeholder="blur" blurDataURL={img64} />
-                    <div className='flex items-center justify-center border-2 border-[#7e59e4] w-40 py-2 rounded-[8px] mt-2 cursor-pointer'>
-                        <i className='text-xl'><TbCameraPlus /></i><p className='text-sm ml-2 font-medium' onClick={() => setIsUploadPhotoOpen(true)}>Change Avatar</p>
-                    </div>
-                </div>
-                <div className='flex flex-col mt-6'>
-                    <label className='text-gray-600 font-medium mb-1 '>
-=======
+
         <div className={`bg-white  ${windowWidth > 1024 && activeSettingButton !== "change-password" || activeSettingButton === "Edit" ? "flex" : "hidden"} w-[95%] sm:w-[90%] md:w-[85%] lg:w-[65%] px-2 2xl:w-[65%] h-[90%] 2xl:h-[86%] border rounded-[10px] flex flex-col items-center shadow-xl relative`}>
-
             {/* --------Profile -------------- */}
-
             <p className='text-xl sm:text-3xl absolute top-2 left-2 lg:hidden cursor-pointer flex items-center justify-center'
                 onClick={() => setActiveSettingButton("")}
             ><IoChevronBackOutline /><span className="text-sm sm:text-base">Back</span></p>
@@ -134,7 +117,7 @@ const EditProfile: React.FC<EditProfileProps> = ({ activeSettingButton, setActiv
                 <p className="text-gray-600 text-sm font-medium">Make sure your infos are correct.</p>
             </div>
             <div className='flex flex-col w-full justify-center items-center mt-3'>
-                <Image src={UserInfo[0]?.photo ? UserInfo[0].photo : ''} width={200} height={200} alt='Profile Image' className='w-24 h-24 sm:w-32 sm:h-32 rounded-[8px] border object-cover' placeholder="blur" blurDataURL={' data:image/jpeg;base64,/9j//gAQTGF2YzYwLjMxLjEwMgD/2wBDAAgEBAQEBAUFBQUFBQYGBgYGBgYGBgYGBgYHBwcICAgHBwcGBgcHCAgICAkJCQgICAgJCQoKCgwMCwsODg4RERT/xABqAAEBAQEAAAAAAAAAAAAAAAAGBAMHAQEBAQAAAAAAAAAAAAAAAAAAAQIQAAEDAwIGAwEAAAAAAAAAAAIDBAEABRIhEUExFAdREyIyBqERAAMBAAIDAQAAAAAAAAAAAAABEQISITGBQQP/wAARCAAJABQDARIAAhIAAxIA/9oADAMBAAIRAxEAPwDg7SLfOfVm4DlgSAgfnfISkf4VYRzmgAFnbhv20Wv5j+oVUNl0xYA9VcMm8q5DkUq26FVfYmnlKKZYpmX2KNIknwrX5ZzraWtLKvbdk9GS5aVqvRCq7jbRur8bYoqbCHTiGRraKk2hQvSSkbR8pT2mdI14VJV1x5a4tvNcvmfCAH//2Q=='} />
+                <Image src={UserInfo[0]?.photo ? UserInfo[0].photo : ''} width={200} height={200} alt='Profile Image' className='w-24 h-24 sm:w-32 sm:h-32 rounded-[8px] border object-cover' placeholder="blur" blurDataURL={img64} />
                 <button className='flex items-center justify-center border-2 border-[#7e59e4] px-2 py-1 rounded-[8px] mt-2'>
                     <i className='text-xl'><TbCameraPlus /></i><p className='text-sm ml-2 font-medium' onClick={() => setIsUploadPhotoOpen(true)}>Change Avatar</p>
                 </button>
@@ -142,7 +125,6 @@ const EditProfile: React.FC<EditProfileProps> = ({ activeSettingButton, setActiv
             <div className="flex flex-col sm:flex-row gap-x-[6%] w-[95%] sm:w-[90%] md:w-[80%]">
                 <div className='flex flex-col mt-3 md:mt-6 w-full sm:w-[47%]'>
                     <label className='text-gray-600 text-sm sm:text-base font-medium mb-1 '>
->>>>>>> 7cb83254bb4eb4b3fce26133cbca28ef5b52eb51
                         Full Name
                     </label>
                     <input
