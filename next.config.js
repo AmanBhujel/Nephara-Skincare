bfe58b26e037de5e29f9a423e973261b2a643266
Evan Sunde <evansunde2@gmail.com>
2024-02-27 00:10:18 +0545

    blog page and cards added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["nephara-skincare.s3.us-east-1.amazonaws.com"]
+        domains: ["nephara-skincare.s3.us-east-1.amazonaws.com","e1.pxfuel.com"]
       },
 }
 
